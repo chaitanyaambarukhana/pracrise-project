a3389f7006660be54077fb96a7c290c03de22daa
Chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2021-03-07 00:34:42 +0530

    Connected to Mongo Atlas and added data in th database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  
+  //Mongo Atlas link
+  "URI": "mongodb+srv://chaitanya:U7xW2aNxZDPlAXMc@cluster0.e8ef8.mongodb.net/books_store?retryWrites=true&w=majority"
 };
